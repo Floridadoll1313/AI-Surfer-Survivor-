d0847a333a53ffc165ab13a08e0673faea011925
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-18 07:00:11 -0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 import SurvivorWorld from "./pages/SurvivorWorld";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/world" element={<SurvivorWorld />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/world" element={<SurvivorWorld />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
