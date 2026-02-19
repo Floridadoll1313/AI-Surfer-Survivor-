c9e0e2b203fa8213ee0abc76f694587d7b0457bf
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-19 07:44:18 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,24 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/Home';
-// Import other pages as you create them
+import Home from './pages/home';
+import Challenges from './pages/challenges';
+import SurvivorWorld from './pages/survivorworld';
+import LessonList from './pages/LessonList';
+import LessonPage from './pages/LessonPage';
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Add more routes here, like /island or /challenges */}
+          <Route path="/challenges" element={<Challenges />} />
+          <Route path="/world" element={<SurvivorWorld />} />
+          <Route path="/lessons" element={<LessonList />} />
+          <Route path="/lessons/:id" element={<LessonPage />} />
         </Routes>
       </Layout>
     </Router>
   );
 }
-
-export default App;
