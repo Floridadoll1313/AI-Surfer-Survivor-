28b677974973ab4a22ac92034f4ac4c087bdc80c
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-19 14:22:08 -0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// REMOVE the index.css line from here!
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  document.body.innerHTML = '<div style="color: white; padding: 20px;">Error: Root element not found! Check index.html</div>';
+} else {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
