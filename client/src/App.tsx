5aab16fc8bcbe4062475a1529bfc9d51ab9f068c
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-19 13:27:10 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,66 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AvatarProvider } from './context/AvatarContext';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 // Page Imports
 import Home from './pages/Home';
-import SurvivorWorld from './pages/SurvivorWorld';
+import Members from './pages/Members';
+import Lessons from './pages/Lessons'; // New Import
 import Profile from './pages/Profile';
-import Shop from './pages/Shop';
+import SurvivorWorld from './pages/SurvivorWorld';
 import Achievements from './pages/Achievements';
 import Leaderboard from './pages/Leaderboard';
-import Credits from './pages/Credits';
 
 function App() {
   return (
-    <AvatarProvider>
+    <AuthProvider>
       <Router>
         <div className="min-h-screen bg-[#0a192f]">
           <Routes>
-            {/* Main Hub */}
+            {/* Public Routes */}
             <Route path="/" element={<Home />} />
+
+            {/* Protected Member Routes */}
+            <Route path="/members" element={
+              <ProtectedRoute>
+                <Members />
+              </ProtectedRoute>
+            } />
             
-            {/* Core Game */}
-            <Route path="/survivor" element={<SurvivorWorld />} />
-            
-            {/* Player Systems */}
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/achievements" element={<Achievements />} />
-            
-            {/* Social & Info */}
-            <Route path="/leaderboard" element={<Leaderboard />} />
-            <Route path="/credits" element={<Credits />} />
+            <Route path="/lessons" element={
+              <ProtectedRoute>
+                <Lessons />
+              </ProtectedRoute>
+            } />
+
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            } />
+
+            <Route path="/world" element={
+              <ProtectedRoute>
+                <SurvivorWorld />
+              </ProtectedRoute>
+            } />
+
+            <Route path="/achievements" element={
+              <ProtectedRoute>
+                <Achievements />
+              </ProtectedRoute>
+            } />
+
+            <Route path="/leaderboard" element={
+              <ProtectedRoute>
+                <Leaderboard />
+              </ProtectedRoute>
+            } />
           </Routes>
         </div>
       </Router>
-    </AvatarProvider>
+    </AuthProvider>
   );
 }
 
