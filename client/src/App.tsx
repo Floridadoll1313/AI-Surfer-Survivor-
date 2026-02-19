1969c11e303a3dce60dbc5320fef851dbdfebd10
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-19 10:18:10 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,32 @@
-import Header from './components/Header'; // Add this import
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AvatarProvider } from './context/AvatarContext';
+import AvatarSelector from './pages/AvatarSelector';
+import Header from './components/Header';
 
-// Inside your return:
-<AvatarProvider>
-  <Router>
-    <div className="min-h-screen bg-[#0a192f]">
-      <Header /> {/* Add this here! */}
-      <Routes>
-        <Route path="/avatar" element={<AvatarSelector />} />
-      </Routes>
-    </div>
-  </Router>
-</AvatarProvider>
+function App() {
+  return (
+    <AvatarProvider>
+      <Router>
+        <div style={{ 
+          minHeight: '100vh', 
+          backgroundColor: '#0a192f',
+          color: '#ffffff' 
+        }}>
+          <Header />
+          <main style={{ padding: '20px' }}>
+            <Routes>
+              {/* This makes the Avatar Selector the home page for now */}
+              <Route path="/" element={<AvatarSelector />} />
+              <Route path="/avatar" element={<AvatarSelector />} />
+              
+              {/* Future routes like /game or /profile will go here */}
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AvatarProvider>
+  );
+}
+
+export default App;
