fc1beeafc7128414acc6c19852a99af7ee847053
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-20 09:59:04 -0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  document.body.innerHTML = '<div style="color: white; padding: 20px;">Error: Root element not found! Check index.html</div>';
-} else {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
