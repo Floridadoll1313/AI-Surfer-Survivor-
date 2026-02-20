c764626ef54317a2a5310bdc5ba0be0b87e3cb85
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-20 10:01:11 -0500

    Create tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#0a192f",
+        cyan: "#22d3ee",
+        gold: "#facc15",
+      },
+    },
+  },
+  plugins: [],
+};
