3a927b5f5728dc3043270c34a61411a6ed6c24c5
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-22 13:06:44 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 import FoundersPage from "./pages/FoundersPage";
 import SailorAnn from "./pages/founders/SailorAnn";
 import StormyGray from "./pages/founders/StormyGray";
@@ -11,12 +14,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+
         <Route path="/founders" element={<FoundersPage />} />
         <Route path="/founders/sailor-ann" element={<SailorAnn />} />
         <Route path="/founders/stormy-gray" element={<StormyGray />} />
         <Route path="/founders/sky-marlin" element={<SkyMarlin />} />
         <Route path="/founders/shannon" element={<ShannonFounder />} />
+
         <Route path="/lore/shannon" element={<ShannonLore />} />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
