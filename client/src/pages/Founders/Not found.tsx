d14e827c258b65f80bded3bdfd4969e7f298dbf5
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-22 13:19:01 -0500

    Create Not found.tsx

---
+++ client/src/pages/Founders/Not found.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+function NotFound() {
+  return (
+    <Layout>
+      <h1>Page Not Found</h1>
+      <p>The tide carried this page away.</p>
+    </Layout>
+  );
+}
+
+export default NotFound;
