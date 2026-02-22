e685698eb73adad4d308ec9b85941e111f15037f
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-22 14:34:29 -0500

    Update FoundersPage.tsx

--- client/src/pages/Founders/FoundersPage.tsx
+++ client/src/pages/Founders/FoundersPage.tsx
@@ -1,5 +1,6 @@
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
+import ScrollReveal from "../components/ScrollReveal";
 import "../GlobalTheme.css";
 
 function FoundersPage() {
@@ -14,25 +15,33 @@ function FoundersPage() {
       <div className="wave-divider"></div>
 
       <div className="card-grid">
-        <a href="/founders/sailor-ann" className="card">
-          <h2>Sailor Ann</h2>
-          <p>The Navigator of Infinite Tides.</p>
-        </a>
+        <ScrollReveal>
+          <a href="/founders/sailor-ann" className="card founder-select cosmic-border">
+            <h2>Sailor Ann</h2>
+            <p>The Navigator of Infinite Tides.</p>
+          </a>
+        </ScrollReveal>
 
-        <a href="/founders/stormy-gray" className="card">
-          <h2>Stormy Gray</h2>
-          <p>The Keeper of Tempests.</p>
-        </a>
+        <ScrollReveal>
+          <a href="/founders/stormy-gray" className="card founder-select cosmic-border">
+            <h2>Stormy Gray</h2>
+            <p>The Keeper of Tempests.</p>
+          </a>
+        </ScrollReveal>
 
-        <a href="/founders/sky-marlin" className="card">
-          <h2>Sky Marlin</h2>
-          <p>The Celestial Guardian.</p>
-        </a>
+        <ScrollReveal>
+          <a href="/founders/sky-marlin" className="card founder-select cosmic-border">
+            <h2>Sky Marlin</h2>
+            <p>The Celestial Guardian.</p>
+          </a>
+        </ScrollReveal>
 
-        <a href="/founders/shannon" className="card">
-          <h2>Shannon</h2>
-          <p>The Architect of the Cosmic Tide.</p>
-        </a>
+        <ScrollReveal>
+          <a href="/founders/shannon" className="card founder-select cosmic-border">
+            <h2>Shannon</h2>
+            <p>The Architect of the Cosmic Tide.</p>
+          </a>
+        </ScrollReveal>
       </div>
     </Layout>
   );
