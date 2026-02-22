02235b3a051f35b968fb57628a3557737eb62e81
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-22 13:17:39 -0500

    Create ShannonFounder.tsx

---
+++ client/src/pages/Founders/ShannonFounder.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/Layout";
+
+function ShannonFounder() {
+  return (
+    <Layout>
+      <h1>Shannon</h1>
+      <p>The visionary creator behind Ocean Tide Drop.</p>
+    </Layout>
+  );
+}
+
+export default ShannonFounder;
