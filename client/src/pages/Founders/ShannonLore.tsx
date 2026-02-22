59aea97db2202925781ad05bff976d1e1ccb596e
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-22 13:18:16 -0500

    Create ShannonLore.tsx

---
+++ client/src/pages/Founders/ShannonLore.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/Layout";
+
+function ShannonLore() {
+  return (
+    <Layout>
+      <h1>Shannon Lore</h1>
+      <p>The mythic story of the creator who shaped the tides.</p>
+    </Layout>
+  );
+}
+
+export default ShannonLore;
