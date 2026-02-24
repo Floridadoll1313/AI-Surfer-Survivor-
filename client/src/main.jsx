21204fbe1e20571f9378d68e22daf44cf33c9982
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-24 14:35:54 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+// Global Systems
 import App from "./App";
+import { ProgressionProvider } from "./source/hooks/ProgressionContext";
+import { LoreProvider } from "./source/hooks/LoreContext";
+import { AnimationProvider } from "./source/hooks/AnimationContext";
+
+// Global Styles
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AnimationProvider>
+        <ProgressionProvider>
+          <LoreProvider>
+            <App />
+          </LoreProvider>
+        </ProgressionProvider>
+      </AnimationProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
