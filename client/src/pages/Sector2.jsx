1b53dcb2989737e658b83733eb0d547fd1fbd0be
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-24 14:48:00 -0500

    Create Sector2.jsx

---
+++ client/src/pages/Sector2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Sector2() {
+  return (
+    <div className="sector-page" data-reveal>
+      <h1>Sector 2: The Crossing</h1>
+      <p>Movement becomes mastery.</p>
+    </div>
+  );
+}
