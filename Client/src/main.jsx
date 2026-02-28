8e3497b61dc91be8e35c3fa4abd2a2c379d3a1e2
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-02-27 19:53:03 -0500

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-// Global Systems
-import App from "./App";
+// Global Systems (only include these if the files actually exist)
 import { ProgressionProvider } from "./source/hooks/ProgressionContext";
 import { LoreProvider } from "./source/hooks/LoreContext";
 import { AnimationProvider } from "./source/hooks/AnimationContext";
 
-// Global Styles
-import "./styles.css";
+// App Root
+import App from "./App.jsx";
+
+// Global Styles (Tailwind + your custom CSS)
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
