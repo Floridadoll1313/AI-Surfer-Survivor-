270528bf3c2cab4f0e65ec40cba16d0a83138011
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-03-10 20:20:10 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+// If your hook is a .js file, use .js; if it's .jsx, use .jsx
+import { ProgressionProvider } from './source/hooks/ProgressionContext.jsx';
 
-// Global Systems (only include these if the files actually exist)
-import { ProgressionProvider } from "./source/hooks/ProgressionContext";
-import { LoreProvider } from "./source/hooks/LoreContext";
-import { AnimationProvider } from "./source/hooks/AnimationContext";
-
-// App Root
-import App from "./App.jsx";
-
-// Global Styles (Tailwind + your custom CSS)
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AnimationProvider>
-        <ProgressionProvider>
-          <LoreProvider>
-            <App />
-          </LoreProvider>
-        </ProgressionProvider>
-      </AnimationProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <ProgressionProvider>
+      <App />
+    </ProgressionProvider>
+  </React.StrictMode>,
 );
