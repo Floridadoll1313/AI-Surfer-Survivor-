d89fbdc77015eb1ec03ea8192f868550710ded13
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-03-16 13:28:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  },
+    outDir: 'dist'
+  }
 });
