2030ba83868549f8f9c8eaa901d2ee88fcdbd869
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-03-19 11:43:50 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Free from './free.jsx';       // Pink Surfboard
-import Members from './members.jsx'; // Blue Surfboard
+import Free from './pages/free.jsx';       // Pink Surfboard
+import Members from './pages/members.jsx'; // Blue Surfboard
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* React handles the display when the URL matches */}
         <Route path="/dashboard-pink" element={<Free />} />
         <Route path="/dashboard-blue" element={<Members />} />
       </Routes>
