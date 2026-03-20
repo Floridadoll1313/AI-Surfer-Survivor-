188792351b0afb54148368d6324049d272010fd9
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-03-20 13:13:28 -0400

    Merge branch 'main' of https://github.com/Floridadoll1313/my-cool-repo

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-<<<<<<< HEAD
-    emptyOutDir: true
-=======
     emptyOutDir: true,
   },
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
     }
->>>>>>> 064f0f3391a6a75a152fe7a81b96c72465a1f367
   }
-}); 
+});
